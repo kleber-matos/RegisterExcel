dda434c3ed440e92d0d0481b92932933e5a66b4c
kleber-matos <94052079+kleber-matos@users.noreply.github.com>
2024-12-07 22:34:26 -0300

    :construction: Em progresso

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Table from "./components/Table";
 export default function App() {
   return (
     <>
+      <h2>Este projeto é apenas um testes de funcionalidades - estudos</h2>
       <Table />
     </>
   );
